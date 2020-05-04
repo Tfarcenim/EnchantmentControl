5ab4af11e26c8fd5bd3e479842963bfaf6e0a609
Tfarcenim <44327798+Gimpler@users.noreply.github.com>
2020-05-04 13:58:08 -0400

    less code, less crashes

--- src/main/java/tfar/enchantmentcontrol/EnchantmentControl.java
+++ src/main/java/tfar/enchantmentcontrol/EnchantmentControl.java
@@ -1,28 +1,47 @@
 package tfar.enchantmentcontrol;
 
+import com.google.common.collect.Lists;
 import net.minecraft.enchantment.Enchantment;
-import net.minecraftforge.fml.ModLoadingContext;
+import net.minecraft.tags.Tag;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.registry.Registry;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.config.ModConfig;
+import tfar.extratags.api.tagtypes.EnchantmentTags;
 
-import java.util.HashSet;
-import java.util.Set;
-
-import static tfar.enchantmentcontrol.ModConfigs.SERVER_SPEC;
+import java.util.Collection;
+import java.util.List;
+import java.util.Random;
+import java.util.stream.Collectors;
 
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod(EnchantmentControl.MODID)
 public class EnchantmentControl {
     public static final String MODID = "enchantmentcontrol";
 
+    public static final Random rand = new Random();
+
     public EnchantmentControl() {
         if(!isMixinInClasspath())
-            throw new IllegalStateException("install mixin bootstrap: https://github.com/LXGaming/MixinBootstrap/releases/download/v1.0.1/MixinBootstrap-1.0.1.jar");
-        ModLoadingContext.get().registerConfig(ModConfig.Type.SERVER, SERVER_SPEC);
+            throw new IllegalStateException("install mixin bootstrap: https://www.curseforge.com/minecraft/mc-mods/mixinbootstrap");
     }
 
-    public static final Set<Enchantment> blackisted_from_trade = new HashSet<>();
-    public static final Set<Enchantment> blackisted_from_loot_tables = new HashSet<>();
+    public static Enchantment filteredRandomTrade(){
+        List<Enchantment> enchantmentList = Registry.ENCHANTMENT.stream().filter(e ->
+                !EnchantmentControl.blacklisted_from_trade.contains(e)).collect(Collectors.toList());
+        //todo 0
+        return enchantmentList.get(EnchantmentControl.rand.nextInt(enchantmentList.size()));
+    }
+
+    public static List<Enchantment> filteredRandomLoot(Collection<Enchantment> enchantments) {
+        return Lists.newArrayList(enchantments).stream().filter(enchantment ->
+                !blackisted_from_loot_tables.contains(enchantment)).collect(Collectors.toList());
+    }
+
+    public static final Tag<Enchantment> blacklisted_from_trade = EnchantmentTags.makeWrapperTag(
+            new ResourceLocation(MODID,"blacklisted_from_trade"));
+
+    public static final Tag<Enchantment> blackisted_from_loot_tables = EnchantmentTags.makeWrapperTag(
+            new ResourceLocation(MODID,"blacklisted_from_loot_tables"));
 
     public static boolean isMixinInClasspath() {
         try {
--- src/main/java/tfar/enchantmentcontrol/mixin/EnchantedBooksForEmeraldsTradeMixin.java
+++ src/main/java/tfar/enchantmentcontrol/mixin/EnchantedBooksForEmeraldsTradeMixin.java
@@ -1,25 +1,17 @@
 package tfar.enchantmentcontrol.mixin;
 
 import net.minecraft.enchantment.Enchantment;
-import net.minecraft.util.registry.Registry;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import tfar.enchantmentcontrol.EnchantmentControl;
-
-import java.util.List;
-import java.util.Random;
-import java.util.stream.Collectors;
 
 @Mixin(targets = "net.minecraft.entity.merchant.villager.VillagerTrades$EnchantedBookForEmeraldsTrade")
 public class EnchantedBooksForEmeraldsTradeMixin {
-  @Redirect(method = "getOffer",at = @At(value = "INVOKE",
-          target = "net/minecraft/util/registry/Registry.getRandom(Ljava/util/Random;)Ljava/lang/Object;"))
-  private Object filter(Registry<Enchantment> registry,Random rand){
 
-    List<Enchantment> enchantmentList = Registry.ENCHANTMENT.stream().filter(e ->
-            !EnchantmentControl.blackisted_from_trade.contains(e)).collect(Collectors.toList());
-
-    return enchantmentList.get(rand.nextInt(enchantmentList.size()));
+  @ModifyVariable(method = "getOffer",at = @At(value = "INVOKE",
+          target = "Lnet/minecraft/util/math/MathHelper;nextInt(Ljava/util/Random;II)I"))
+  private Enchantment filter(Enchantment old){
+  	return EnchantmentControl.filteredRandomTrade();
   }
 }
